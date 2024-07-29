d5bdeaba6917e3b7b9a33cab19e39fdc84a4c12f
Aadit_17 <148260267+CosmicArcher17@users.noreply.github.com>
2024-07-29 15:26:55 +0530

    Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void is(int a[],int n){
-    int i,j,t;
-    j=n-1;
-    i=j-1;
-    int comp=0;
-    int key=a[j];
-    while(i>=0 && a[i]>key){
-        comp++;
-        a[i+1]=a[i];
-        i--;
+void ins(int a[],int n){
+    int i,j;
+    for(j=1;j<n;j++){
+        i=j-1;
+        int key=a[j];
+        while(i>=0 && a[i]>key){
+            a[i+1]=a[i];
+            i--;
+        }
+        a[i+1]=key;
     }
-    a[i+1]=key;
-
-    cout<<"Insertion Sort comparisons: "<<comp<<endl;
     for(i=0;i<n;i++){
-        cout<<a[i]<<" ";
+        cout<<a[i]<<endl;     
     }
-    cout<<endl;
 }
 
 int main(){
