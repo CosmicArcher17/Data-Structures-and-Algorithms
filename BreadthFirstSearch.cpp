ff5f2fb7da01bbddfa8b1da80644fea10f9b7a5a
Aadit_17 <148260267+CosmicArcher17@users.noreply.github.com>
2025-03-22 11:16:43 +0530

    BreadthFirstSearch.cpp

--- BreadthFirstSearch.cpp
+++ BreadthFirstSearch.cpp
@@ -1,87 +1,10 @@
 #include <iostream>
-#include <stdlib.h>
 #include <queue>
 #include <vector>
 using namespace std;
 
 int nvert, nedge;
 
-struct Node
-{
-    int data;
-    struct Node *link;
-} *temp;
-Node *head = NULL;
-
-void createnode(Node *node, int value)
-{
-    if (node == NULL)
-    {
-        node = (struct Node *)malloc(sizeof(struct Node));
-        node->data = value;
-        node->link = NULL;
-        head = node;
-        return;
-    }
-    else if (node->link == NULL)
-    {
-        struct Node *p = (struct Node *)malloc(sizeof(struct Node));
-        p->data = value;
-        p->link = NULL;
-        node->link = p;
-        return;
-    }
-    else if (node->link != NULL)
-    {
-        node = node->link;
-        createnode(node, value);
-    }
-}
-void disp(struct Node *head)
-{
-    temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->link;
-    }
-}
-
-void insertb(int val)
-{
-    Node *newnode = (struct Node *)malloc(sizeof(struct Node));
-    newnode->data = val;
-    newnode->link = head;
-    head = newnode;
-}
-
-void inserte(int val)
-{
-    temp = head;
-    while (temp->link != NULL)
-    {
-        temp = temp->link;
-    }
-    createnode(temp, val);
-}
-
-void delnode(int val)
-{
-    temp = head;
-    if (temp->data == val)
-    {
-        temp = temp->link;
-        head = temp;
-    }
-    else
-    {
-        while (temp->link->data != val)
-        {
-            temp = temp->link;
-        }
-        temp->link = temp->link->link;
-    }
-}
 void bfs(vector<vector<int>> &am, int start)
 {
     queue<int> q;
